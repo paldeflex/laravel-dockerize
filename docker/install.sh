328a84c2c64ce886cf9ad85d138e5d9ebefed477
paldeflex <paldeflex@gmail.com>
2025-05-10 14:37:09 +0500

    Поправил установочный скрипт

--- docker/install.sh
+++ docker/install.sh
@@ -3,14 +3,16 @@ set -e
 
 docker compose up --build -d
 
-if [ ! -f ./src/.env ]; then
-  cp ./src/.env.example ./src/.env
+sudo chown -R $(id -u):$(id -g) ../src
+
+if [ ! -f ../src/.env ]; then
+  cp ../src/.env.example ../src/.env
   echo "Created src/.env"
 fi
 
 docker compose exec php-cli composer install
 
-sudo chmod -R 777 ./src/storage
+sudo chmod -R 777 ../src/storage
 
 docker compose exec php-cli bash -c "php artisan key:generate"
 
